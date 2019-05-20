89e02e019c9681b42809c34ee59db3b15d3fe783
Just Burrow <just.burrow@lul.kr>
2019-05-20 23:15:39 +0900

    ADD : `Article` 검증.
    issue : https://github.com/JustBurrow/kobalttown/issues/11
    time : 1h 29m

--- article-domain/src/main/java/kr/lul/kobalttown/article/domain/Article.java
+++ article-domain/src/main/java/kr/lul/kobalttown/article/domain/Article.java
@@ -1,8 +1,8 @@
 package kr.lul.kobalttown.article.domain;
 
 import kr.lul.kobalttown.account.domain.Account;
-import kr.lul.kobalttown.common.util.AssertionException;
 import kr.lul.kobalttown.common.util.Creatable;
+import kr.lul.kobalttown.common.util.ValidationException;
 
 import static java.lang.String.format;
 
@@ -11,6 +11,13 @@ import static java.lang.String.format;
  * @since 2019-03-31
  */
 public interface Article extends Creatable {
+  String ATTR_ID = "id";
+  String ATTR_TITLE = "title";
+  String ATTR_SUMMARY = "summary";
+  String ATTR_BODY = "body";
+  String ATTR_CREATOR = "creator";
+  String ATTR_CREATED_AT = "createdAt";
+
   int TITLE_MAX_LENGTH = 20;
   int SUMMARY_MAX_LENGTH = 64;
   int BODY_MAX_LENGTH = 65_535;
@@ -19,14 +26,16 @@ public interface Article extends Creatable {
    * 문자열이 제목으로 사용할 수 있는지 확인한다.
    *
    * @param title 제목.
+   *
+   * @throws ValidationException 검증 실패.
    */
-  static void validateTitle(String title) {
+  static void validateTitle(String title) throws ValidationException {
     if (null == title) {
-      throw new AssertionException("title is null.");
+      throw new ValidationException(ATTR_TITLE, "title is null.");
     } else if (title.isEmpty()) {
-      throw new AssertionException("title is empty.");
+      throw new ValidationException(ATTR_TITLE, "title is empty.");
     } else if (TITLE_MAX_LENGTH < title.length()) {
-      throw new AssertionException(format("title is too long : length=%d, max=%d",
+      throw new ValidationException(ATTR_TITLE, format("title is too long : length=%d, max=%d",
           title.length(), TITLE_MAX_LENGTH));
     }
   }
@@ -35,15 +44,17 @@ public interface Article extends Creatable {
    * 요약으로 사용할 수 있는 문자열인지 확인한다.
    *
    * @param summary 요약.
+   *
+   * @throws ValidationException 검증 실패.
    */
-  static void validateSummary(String summary) {
+  static void validateSummary(String summary) throws ValidationException {
     if (null == summary) {
-      throw new AssertionException("summary is null.");
+      throw new ValidationException(ATTR_SUMMARY, "summary is null.");
     } else if (summary.isEmpty()) {
-      throw new AssertionException("summary is empty.");
+      throw new ValidationException(ATTR_SUMMARY, "summary is empty.");
     } else if (SUMMARY_MAX_LENGTH < summary.length()) {
-      throw new AssertionException(format("summary is too long : length=%d, max=%d",
-          summary.length(), SUMMARY_MAX_LENGTH));
+      throw new ValidationException(ATTR_SUMMARY,
+          format("summary is too long : length=%d, max=%d", summary.length(), SUMMARY_MAX_LENGTH));
     }
   }
 
@@ -51,12 +62,14 @@ public interface Article extends Creatable {
    * 본문으로 사용할 수 있는 문자열인지 확인한다.
    *
    * @param body 본문.
+   *
+   * @throws ValidationException 검증 실패.
    */
-  static void validateBody(String body) {
+  static void validateBody(String body) throws ValidationException {
     if (null == body) {
-      throw new AssertionException("body is null.");
+      throw new ValidationException(ATTR_BODY, "body is null.");
     } else if (body.isEmpty()) {
-      throw new AssertionException("body is empty.");
+      throw new ValidationException(ATTR_BODY, "body is empty.");
     }
   }
 
