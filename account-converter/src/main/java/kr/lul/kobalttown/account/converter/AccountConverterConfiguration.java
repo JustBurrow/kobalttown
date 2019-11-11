1894d7acd0e3465ed4a37ee77642ffd5f3e86ac5
Just Burrow <just.burrow@lul.kr>
2019-11-11 23:31:35 +0900

    RESET

--- account-converter/src/main/java/kr/lul/kobalttown/account/converter/AccountConverterConfiguration.java
+++
@@ -1,8 +0,0 @@
-package kr.lul.kobalttown.account.converter;
-
-/**
- * @author justburrow
- * @since 2019-02-27
- */
-public class AccountConverterConfiguration {
-}
--- common-util/src/main/java/kr/lul/kobalttown/common/util/SimpleToString.java
+++
@@ -1,9 +0,0 @@
-package kr.lul.kobalttown.common.util;
-
-/**
- * @author justburrow
- * @since 2019-02-28
- */
-public interface SimpleToString {
-  String simpleToString();
-}
