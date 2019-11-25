c566b0716f6726efa6c596d56d0d6112e901f7eb
Just Burrow <just.burrow@lul.kr>
2019-11-25 23:42:36 +0900

    TEMP : 웹 모듈 단위테스트의 `401`에러 수정.

--- account/account-web/src/test/java/kr/lul/kobalttown/account/web/AccountWebTestConfiguration.java
+++ account/account-web/src/test/java/kr/lul/kobalttown/account/web/AccountWebTestConfiguration.java
@@ -1,30 +1,17 @@
 package kr.lul.kobalttown.account.web;
 
-import kr.lul.common.util.SystemTimeProvider;
-import kr.lul.common.util.TimeProvider;
+import kr.lul.kobalttown.configuration.bean.BeanConfiguration;
 import kr.lul.kobalttown.configuration.data.jpa.JpaConfiguration;
 import kr.lul.kobalttown.configuration.web.WebMvcConfiguration;
-import kr.lul.support.spring.security.crypto.PasswordEncoderSecurityEncoder;
-import kr.lul.support.spring.security.crypto.SecurityEncoder;
+import kr.lul.kobalttown.configuration.web.WebSecurityConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * @author justburrow
  * @since 2019/11/24
  */
 @SpringBootApplication(scanBasePackageClasses = AccountWebAnchor.class)
-@Import({JpaConfiguration.class, WebMvcConfiguration.class})
+@Import({JpaConfiguration.class, WebMvcConfiguration.class, WebSecurityConfiguration.class, BeanConfiguration.class})
 public class AccountWebTestConfiguration {
-  @Bean
-  public TimeProvider timeProvider() {
-    return new SystemTimeProvider();
-  }
-
-  @Bean
-  public SecurityEncoder securityEncoder() {
-    return new PasswordEncoderSecurityEncoder(new BCryptPasswordEncoder());
-  }
 }
