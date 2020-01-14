6af5d2f491e281f1af1d43657edccbec33458a48
Just Burrow <just.burrow@lul.kr>
2020-01-15 00:51:32 +0900

    DELETE : 불필요한 의존성 삭제.

--- account/account-converter/src/test/java/kr/lul/kobalttown/account/converter/AccountConverterTestConfiguration.java
+++ account/account-converter/src/test/java/kr/lul/kobalttown/account/converter/AccountConverterTestConfiguration.java
@@ -1,7 +1,6 @@
 package kr.lul.kobalttown.account.converter;
 
 import kr.lul.kobalttown.configuration.bean.BeanConfiguration;
-import kr.lul.kobalttown.configuration.data.jpa.JpaConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
@@ -10,6 +9,6 @@ import org.springframework.context.annotation.Import;
  * @since 2019/11/24
  */
 @SpringBootApplication(scanBasePackageClasses = AccountConverterAnchor.class)
-@Import({BeanConfiguration.class, JpaConfiguration.class})
+@Import({BeanConfiguration.class})
 public class AccountConverterTestConfiguration {
 }
